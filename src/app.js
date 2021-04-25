18d2ac231159ff8b6531ffb6087f4466ada8ca53
NoAi-coder <80481550+NoAi-coder@users.noreply.github.com>
2021-04-25 13:33:45 +0200

    Added search city function on JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//Display current day & time
 function getCurrentTime() {
   let now = new Date();
   let days = [
@@ -24,3 +25,18 @@ function getCurrentTime() {
 }
 
 getCurrentTime();
+
+//Search city function when clicking button
+function submitSearch(event) {
+  event.preventDefault();
+  let yourCity = document.querySelector("#search-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = yourCity.value;
+
+  let apiKey = "27ddfc7325668fadfd863bab705e25e8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${yourCity.value}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(submitSearch, showTemp);
+}
+
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", submitSearch);
