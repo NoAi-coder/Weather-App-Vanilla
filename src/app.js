96de4a19f7e5f0c437b5831e49b389df4733b697
NoAi-coder <80481550+NoAi-coder@users.noreply.github.com>
2021-04-25 11:43:21 +0200

    Updated currentTime function on JS

---
+++ src/app.js
@@ -0,0 +1,26 @@
+function getCurrentTime() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let currentTime = document.querySelector(".current");
+  return (currentTime.innerHTML = `Now: ${day} at ${hour}:${minutes}`);
+}
+
+getCurrentTime();
